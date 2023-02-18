ca2cc1a99372553258d7042cf7e8ea3a147c4cc3
kashif0426 <82613468+kashif0426@users.noreply.github.com>
2023-02-18 08:36:10 +0100

    Bank app python

--- expanseapp/urls.py
+++ expanseapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
-    path('expense', expense),
-    path('del/<int:id>', del_views, name = "del")
+    path('expense', expense, name = "expense"),
+    path('del/<int:id>', del_views, name = "del"),
+    path('edit/<int:id>', edit_views, name = "edit"),
 ]
--- expanseapp/views.py
+++ expanseapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import *
 from .models import *
+from .forms import *
 # Create your views here.
 def expense(request):
     all_expense = Expense.objects.all()
@@ -14,4 +16,20 @@ def del_views(request,id):
     print(id)
     obj = Expense.objects.get(id=id)
     obj.delete()
-    pass
+    messages.error(request, "deleted succesfuly")
+    return redirect('expense')
+
+
+def edit_views(request,id):
+    print(id)
+    obj = Expense.objects.get(id=id)
+    print(obj.purpose)
+    if request.method == "POST":
+        form = ExpenseForm(request.POST, instance = obj)
+        print(form)
+        if form.is_valid():
+            form.save()
+            return redirect("expense")
+    form = ExpenseForm(instance = obj)
+    context = {"form": form }
+    return render(request, "edit.html", context)
